f672ee91d2113495f658b5fa5ef5f2264b4f2906
Keckich <nik.malashko@mail.ru>
2024-12-10 22:49:13 +0100

    updated Access denied page layout

--- CurrencyTracker/ClientApp/src/app/features/access-denied/access-denied.component.ts
+++ CurrencyTracker/ClientApp/src/app/features/access-denied/access-denied.component.ts
@@ -1,10 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatCard, MatCardActions, MatCardContent, MatCardTitle, MatCardHeader } from '@angular/material/card';
+
 @Component({
   selector: 'app-access-denied',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardActions,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    MatButton,
+    MatIcon,
+  ],
   templateUrl: './access-denied.component.html',
   styleUrl: './access-denied.component.css'
 })
